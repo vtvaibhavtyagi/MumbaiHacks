f5ad6a7dd1e71d4ed996e4f3862b6e21b29cd4f8
maheshkutty <bosskutty17@gmail.com>
2023-06-03 21:24:39 +0530

    feat: added ml model for carn detection

--- app/src/service/vehicleInfo.py
+++ app/src/service/vehicleInfo.py
@@ -5,13 +5,100 @@ from datetime import datetime, time
 from typing import List, Optional
 from pydantic import BaseModel
 from pydantic.types import UUID4, constr
+import cv2
+import numpy as np
+from local_utils import detect_lp
+from os.path import splitext, basename
+from keras.models import model_from_json
+import glob
+import pytesseract
+from PIL import Image
+from local_utils import detect_lp
 
 from src.model.vehicle import extractedInfo
 
 
+def countVehicles(name):
+    image = Image.open(name)
+    image = image.resize((450,250))
+    image_arr = np.array(image)
+    grey = cv2.cvtColor(image_arr,cv2.COLOR_BGR2GRAY)
+    Image.fromarray(grey)
+    blur = cv2.GaussianBlur(grey,(5,5),0)
+    Image.fromarray(blur)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+    closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel) 
+    Image.fromarray(closing)
+    car_cascade_src = '../ml/cars.xml'
+    car_cascade = cv2.CascadeClassifier(car_cascade_src)
+    cars = car_cascade.detectMultiScale(closing, 1.1, 1)
+    cnt = 0
+    for (x,y,w,h) in cars:
+        cv2.rectangle(image_arr,(x,y),(x+w,y+h),(255,0,0),2)
+        cnt += 1
+    return cnt
+
+def load_model(path):
+    try:
+        path = splitext(path)[0]
+        print(path)
+        with open('%s.json' % path, 'r') as json_file:
+            model_json = json_file.read()
+        model = model_from_json(model_json, custom_objects={})
+        model.load_weights('/content/drive/MyDrive/cars_detection/wpod-net.h5')
+        print("Loading model successfully...")
+        return model
+    except Exception as e:
+        print(e)
+    
+
+def preprocess_image(image_path,resize=False):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img / 255
+    if resize:
+        img = cv2.resize(img, (224,224))
+    return img
+
+def get_plate(image_path, wpod_net, Dmax=608, Dmin=256):
+    vehicle = preprocess_image(image_path)
+    ratio = float(max(vehicle.shape[:2])) / min(vehicle.shape[:2])
+    side = int(ratio * Dmin)
+    bound_dim = min(side, Dmax)
+    _ , LpImg, _, cor = detect_lp(wpod_net, vehicle, bound_dim, lp_threshold=0.5)
+    return LpImg, cor
+
+# def countCar():
+#     class_name = []
+#     with open('../ml/classes.txt', 'r') as f:
+#         class_name = [cname.strip() for cname in f.readlines()]
+#     # print(class_name)
+#     net = cv2.dnn.readNet('yolov4.weights', 'yolov4.cfg')
+#     model = cv2.dnn_DetectionModel(net)
+#     model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
+
+def extractCarColor(name):
+    pass
+
 def extractVehicleInfo(image: UploadFile):
-    veh = extractedInfo()
-    
-    return veh
-    
+    name = '../img/Cars9.png'
+    cnt = countVehicles(name)
+    if cnt > 0:
+        wpod_net_path = "../ml/wpod-net"
+        wpod_net = load_model(wpod_net_path)
+        img_path = glob.glob(name)
+        LpImg,cor = get_plate(img_path[0], wpod_net)
+        print("Coordinate of plate(s) in image: \n", cor)
+        plate_image = cv2.convertScaleAbs(LpImg[0], alpha=(255.0))
+        number = pytesseract.image_to_string(plate_image)
+        return {
+            "isVehicleExit": True,
+            "vehicleNumber": number,
+            "color": "" 
+        }
+    return {
+        "isVehicleExit": True,
+        "vehicleNumber": number,
+        "color": None 
+    }
     
